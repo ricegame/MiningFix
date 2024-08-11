347778988649d27230f1936120e8d33e1de6c21c
rice.who <100787688+ricedotwho@users.noreply.github.com>
2024-08-11 21:51:35 +1000

    what the sigma

--- src/main/java/ricedotwho/mf/Reference.java
+++
@@ -1,7 +0,0 @@
-package ricedotwho.mf;
-
-public class Reference {
-	public static final String MOD_ID = "mf";
-	public static final String MOD_NAME = "Mining Fix";
-	public static final String VERSION = "1.0.1";
-}
